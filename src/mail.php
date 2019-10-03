841674760f6b6bb069c801d74eb5b78c043c1aff
Mikhail Brazhnik <mikbrazh@gmail.com>
2019-10-03 17:27:33 +0300

    mail.php modified

--- src/mail.php
+++ src/mail.php
@@ -15,12 +15,13 @@ $mail = new PHPMailer(true);
 try {
     //Server settings
     $mail->CharSet = 'utf-8';
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
     // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'mikkorab@gmail.com';                     // SMTP username
-    $mail->Password   = '*';                               // SMTP password
+    $mail->Password   = 'good365mood';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
